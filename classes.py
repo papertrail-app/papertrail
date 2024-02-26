b7481635d118d5ef668845470260e8d74dfb90c0
sohalsdr <sohal@sohalsdr.com>
2024-02-26 12:05:11 -0500

    Added PaperTrailDocument class skeleton

---
+++ classes.py
@@ -0,0 +1,12 @@
+class PaperTrailDocument:
+    def __init__(self, password: str):
+        pass
+
+    def __init__(self, document: File):
+        pass
+
+    def decrypt(self, password: str) -> file: File:
+        pass
+
+    def get_pdf(self) -> file: File:
+        pass
