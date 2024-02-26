e7ef8993d702fc3b451a1bac801b58ab277d2313
sohalsdr <sohal@sohalsdr.com>
2024-02-26 11:35:56 -0500

    Fixed shell.nix and successfully reached Hello World

--- shell.nix
+++ shell.nix
@@ -1,5 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ git gnumake comrak python3 ];
+    nativeBuildInputs = with pkgs.buildPackages; [ 
+    	git
+    	gnumake
+    	python3
+    ];
+
+    LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
 }
