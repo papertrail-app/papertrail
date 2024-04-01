0b32fed25b68ac703e76ea632df12aff83437203
sohalsdr <sohal@sohalsdr.com>
2024-03-31 21:38:34 -0400

    Modified import in papertraildriver

--- src/papertrail/papertraildriver.py
+++ src/papertrail/papertraildriver.py
@@ -2,7 +2,7 @@ import base64
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-from papertraildocument import PaperTrailDocument
+from papertrail.papertraildocument import PaperTrailDocument
 
 class PaperTrailDriver:
     def __init__(self):
