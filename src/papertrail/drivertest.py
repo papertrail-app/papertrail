ad8ce7c35e0af4332bf0edd0356b64ac8e5a7acd
sohalsdr <sohal@sohalsdr.com>
2024-03-31 21:45:49 -0400

    Removed test files from module source

--- src/papertrail/drivertest.py
+++
@@ -1,7 +0,0 @@
-from papertraildriver import PaperTrailDriver
-
-
-driver = PaperTrailDriver()
-
-driver.encrypt('password', './testimg.png', './papertrail_drivertest.pdf')
-driver.decrypt('password', './papertrail_drivertest.pdf', 'imgtest_out.png')
