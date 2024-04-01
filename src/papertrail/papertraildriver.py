68791e4e36ed1767924a7509496845d88332d1c3
sohalsdr <sohal@sohalsdr.com>
2024-03-31 23:32:13 -0400

    Modified driver to take designator as a parameter for encryption

--- src/papertrail/papertraildriver.py
+++ src/papertrail/papertraildriver.py
@@ -9,19 +9,18 @@ class PaperTrailDriver:
     def __init__(self):
         pass
 
-    def encrypt(self, password: str, data_path: str, dest_path: str):
+    def encrypt(self, password: str, data_path: str, dest_path: str, designator: str):
         key = self.__derive_key(password)
         with open(data_path, mode="rb") as f:
             data = f.read()
             f.close()
-        pt_doc = PaperTrailDocument(key=key, designator="drivertest", document=dest_path, data=data)
+        pt_doc = PaperTrailDocument(key=key, designator=designator, document=dest_path, data=data)
         pt_doc.encrypt()
         document = pt_doc.get_document()
         return dest_path
 
     def decrypt(self, password: str, document_path: str, dest_path: str) -> str:
         key = self.__derive_key(password)
-        # TODO: extract designator from document_path just for completeness
         pt_doc = PaperTrailDocument(key=key, designator="", document=document_path)
         pt_doc.decrypt()
         data = pt_doc.get_data()
